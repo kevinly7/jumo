998eaa017e31227ab2a940cb141fc899e7529a0d
Howard Lin <hhowardlin@msn.com>
2015-04-13 17:18:09 -0700

    PDO Class

---
+++ pdoCalls.php
@@ -0,0 +1,23 @@
+<?php 
+$conn = new PDO('mysql:host=howarddatabase.c5n7l5fbfxta.us-west-2.rds.amazonaws.com;dbname=ICADB', 'info344user', '<password>'); 
+//Get Players
+/*$statement = $conn->prepare("Select * from tblPLAYER");
+$statement -> execute(array(':name' =>$name, ':firstName' => $firstName, ':lastName' => $lastName));
+$data = $statement->fetchAll();*/
+$playerArray = Array();
+$i = 0;
+foreach($conn->query("Select * from tblPLAYER") as $row) {
+	$player = new Player($row['PlayerName'], $row['PlayerContact'], $row['PlayerID']);
+	$playerArray[$i] = $player;
+	$i++;
+}
+
+$groupArray = Array();
+$j = 0;
+foreach($conn->query("Select * from tblGROUP") as $row) {
+	$group = new Group($row['CoachName'], $row['CoachContact'], $row['GroupID'], $row['GroupName']);
+	$groupArray[$i] = $group;
+	$j++;
+}
+
+?>
