9778a5945a8b887d87ab87920400bf6135f85648
Kevin Yang <kevinjyang126@gmail.com>
2015-04-21 16:04:24 -0700

    almost completely done with this page

--- edit_students.php
+++ edit_students.php
@@ -18,38 +18,108 @@
         <div>
 
 
+ <?php  
+            include ('database.php');
+            include ('Player.php');
+
+            ?>
+
+            <h1>Groups</h1>
             <form action = "edit_students.php" method = "POST">
-                 <label for='formCountries[]'>Select the countries that you have visited:</label><br>
-                <select multiple="multiple" name="formCountries[]">
-                    <option value="US">United States</option>
-                    <option value="UK">United Kingdom</option>
-                    <option value="France">France</option>
-                    <option value="Mexico">Mexico</option>
-                    <option value="Russia">Russia</option>
-                    <option value="Japan">Japan</option>
+                <select name="p2">
+                    <option></option>
+
+<?php 
+
+      foreach($connection->query("Select * from tblGROUP") as $row) {?>
+
+           <option value = <?php echo $row['GroupID'] ?> >
+                <span>
+                    <?php 
+                        echo $row['GroupName'];
+ 
+                    ?>
+                </span>
+                       </br>
+                      </option>
+ 
+                     <?php }?>
+
+
+
+        ?>
+
+                </select>
+
+
+            <a href="settings.html"><button id="createGroups">Create a Group</button></a>
+       
+                 <label for='formStudents[]'>Select the countries that you have visited:</label><br>
+                <select multiple="multiple" name="formStudents[]">
+                     <?php 
+
+
+                     foreach($connection->query("Select * from tblPLAYER") as $row) {
+                       $playerObject = new Player($row['PlayerName'], $row['PlayerID'], $row['PlayerContact']); ?>
+
+           <option value = <?php $playerObject ?>>
+                <span>
+                    <?php 
+                      
+                     
+                       echo $playerObject ->getName();
+ 
+                    ?>
+                </span>
+                       </br>
+                      </option>
+ 
+                     <?php }?>
+ 
                 </select>
 
             <input name = "formSubmit" id = "formSubmit" type="submit" value="Test Submission">
+            
+
             </form>
+
+
+
+
+            <a href="month_view.html"><button id="submit">Submit</button></a>
+
 
 
 <?php
 if(isset($_POST['formSubmit'])) 
 {
-  $aCountries = $_POST['formCountries'];
+
+echo '</br>' . $_POST['p2'] . '</br>';
+$groupid = $_POST['p2'];
+
+  $aStudents = $_POST['formStudents'];
    
-  if(!isset($aCountries)) 
+  if(!isset($aStudents)) 
   {
     echo("<p>You didn't select any countries!</p>\n");
   } 
   else
   {
-    $nCountries = count($aCountries);
+
+ /*$statement = $connection->prepare ("INSERT INTO tblPLAYER_GROUP (PlayerID, GroupID) VALUES (:playerid, :groupid)"); */
+  
+
+
+    $nStudents = count($aStudents);
      
-    echo("<p>You selected $nCountries countries: ");
-    for($i=0; $i < $nCountries; $i++)
+    echo("<p>You selected $nStudents countries: ");
+    for($i=0; $i < $nStudents; $i++)
     {
-      echo($aCountries[$i] . " ");
+        $player = $aStudents[$i];
+      echo($player->getName() . " ");
+
+    
+       /* $statement -> execute(array(':groupid' => $groupid, ':playerid' => $coach)); */
     }
     echo("</p>");
   }
@@ -59,67 +129,6 @@ if(isset($_POST['formSubmit']))
 
 
 
-
-
-
-
-
-
-
-            <h1>Groups</h1>
-            <form action = "edit_students.php" method = "POST">
-                <select name="p2">
-                    <option></option>
-                    <option id="subgroup" value = "1">Hurdles</option>
-                    <option value = "2">Long Jump</option>
-                    <option value = "3">Sprints</option>
-                    <option value = "4">Pole Vault</option>
-                </select>
-
-
-
-            
-
-
-            <a href="settings.html"><button id="createGroups">Create a Group</button></a>
-       
-
-            <div class="subgroup">
-
-               <select multiple>
-                 
-                   
- 
-            <?php  
-            include ('database.php');
- 
-            foreach($connection->query("Select * from tblPLAYER") as $row) {?>
-
-           <option >
-                <span>
-                    <?php 
-                        echo $row['PlayerName'];
- 
-                    ?>
-                </span>
-                       </br>
-                      </option>
- 
-                     <?php }?>
- 
- 
-+                   
-                </select>
-            </div>
-
-
-            <input id = "addStudents" type="submit" value="Add to Group">
-            </form>
-
-
-
-
-            <a href="month_view.html"><button id="submit">Submit</button></a>
         </div>
     </body>
 </html>
