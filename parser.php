6e86b9dd354e1abf2e105ea4e6642cb911672f7a
Kevin Yang <kevinjyang126@gmail.com>
2015-04-21 21:21:12 -0700

    finished connecting all the fields in the text file

--- parser.php
+++ parser.php
@@ -16,46 +16,54 @@ foreach($rows as $row => $data)
 	$month = $row_data[2];
 	$year = $row_data[3];
 	$num_length = strlen((string)$month);
-	$first = $row_data[4];
 	if ($num_length < 2){
 		$month = '0'.$month;
 	}
 
-	if ($first = 0) {
-		$first = '';
-	}
-
 	$row_data = explode(' ', $data);
 	$date = $day.$month.$year;
-	$groupID = $first . $row_data[5]. $row_data[6] . $row_data[7];
+
+	$groupID = $row_data[4] . $row_data[5]. $row_data[6] . $row_data[7];
 	
-	$size = sizeof($practiceArray[$groupID]);
+	if (!array_key_exists($groupID, $practiceArray)) {
+		$practiceArray[$groupID][0] = $row_data[0];
 
-	if ($size == null){
-		$size = 0;
-	}
-
-	$practiceArray[$groupID][$size] = $row_data[0];
-
-	$count++;
-
-	if ($count == 1) {
-		$begin = $row_data[0];
-	} else if ($count == 2){
-		$end = $row_data[0];
 	} else {
-		$count = 0;	
+		$size = sizeof($practiceArray[$groupID]);
+		$practiceArray[$groupID][$size] = $row_data[0];
+		
+	
+	if ($size % 2 == 1 ) {
+		$begin = $practiceArray[$groupID][$size-1];
+		$end = $practiceArray[$groupID][$size];
+		echo $groupID . '</br>';
+		echo $begin . '</br>';
+		echo $end . '</br>' . '</br>';
+		
+		$statement -> execute(array(':dateid' => 1, 
+		':groupid' => $groupID, 
+		':practicetypeid' => 1, 
+		':starttime' => $begin,
+		':endtime' => $end));
+
+		$query = $connection->query("Select PracticeID from tblPRACTICE WHERE GroupID = $groupID AND StartTime = '$begin'");
+		$practiceid = $query->fetch(PDO::FETCH_ASSOC);
+
+
+		foreach($connection->query("Select * from tblPLAYER_GROUP pg WHERE pg.GroupID = $groupID") as $row) {
+			$statement2 -> execute(array(':playerid' => $row['PlayerID'], 
+		':practiceid' => $practiceid['PracticeID']));
+
+		}
+
+	} 
+		
+	
+	
 	}
-	//get row data
-	echo $row_data[0];
-	echo '<br />';
-	echo $date;
-	echo '<br />';
-	echo $groupID;
-	echo '<br />';
-	echo '<br />';
 }
 echo var_dump($practiceArray);
+
 	/*$statement -> execute(array(':dateid' => 1, 
 		':groupid' => $groupID, 
 		':practicetypeid' => 1, 
