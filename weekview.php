b01a7ccda8049c7dfc9cb856d0a9849f49a2a148
Kevin Yang <kevinjyang126@gmail.com>
2015-04-22 20:27:29 -0700

    filters by week

--- weekview.php
+++ weekview.php
@@ -69,7 +69,29 @@ $month = $pieces[1];
 $nextmonth = $month+1;
 $week = $_POST['weekSelect'];
 
+if ($week == 1) {
+	$week = '01';
+} else if ($week == 2) {
+	$week = '08';
+} else if ($week == 3){
+	$week = '15';
+} else {
+	$week = '22';
+}
 
+$num_length = strlen((string)$month);
+if($num_length < 2) {
+    // Pass
+    $month = '0'.$month;
+} 
+
+
+$date = $year.$month.$week;
+echo $date . '</br>';
+
+$weekrange = week_range($date);
+$startday = $weekrange[0];
+$endday = $weekrange[1];
 
 foreach($connection->query("Select StartTime, EndTime, PracticeTypeName, DateName, PlayerName
 from tblPRACTICE p
@@ -81,27 +103,28 @@ join tblPLAYER_PRACTICE pp
 on pp.PracticeID = p.PracticeID
 join tblPLAYER pl
 on pp.PlayerID = pl.PlayerID
-WHERE DATE(d.DateName) BETWEEN '$year-$month-01' AND '$year-$nextmonth-01'") as $row) {
+WHERE DATE(d.DateName) BETWEEN '$startday' AND '$endday'") as $row) {
 	echo $row['DateName'] . ' ' . $row['PlayerName']. ' ' . $row['StartTime'] . ' to ' . $row['EndTime'];
 	echo '</br>';
 }
 
 }
-
-/* foreach($connection->query("Select StartTime, EndTime, PracticeTypeName, DateName, PlayerName
-from tblPRACTICE p
-join tblPRACTICE_TYPE pt
-on p.PracticeTypeID = pt.PracticeTypeID
-join tblDATE d
-on p.DateID = d.DateID
-join tblPLAYER_PRACTICE pp
-on pp.PracticeID = p.PracticeID
-join tblPLAYER pl
-on pp.PlayerID = pl.PlayerID") as $row) {
-	echo $row['DateName'] . ' ' . $row['PlayerName']. ' ' . $row['StartTime'] . ' to ' . $row['EndTime'];
-	echo '</br>';
- } */
 
 ?>
     </body>
 </html>
+
+
+
+<?php 
+function week_range($date) {
+  $ts = strtotime($date);
+  $start = (date('w', $ts) == 0) ? $ts : strtotime('last sunday', $ts);
+  return array(date('Y.m.d', $start), 
+  	date('Y.m.d', strtotime('next saturday', $start)));
+}
+
+
+
+?>
+
