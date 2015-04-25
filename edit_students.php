18b132bf0030eda5c8ada326b6b99f4ff6cc54e6
Tuvshin Tulga <mbmtuvshin@yahoo.com>
2015-04-25 16:49:53 -0700

    no message

--- edit_students.php
+++ edit_students.php
@@ -1,10 +1,7 @@
 <?php
-
-    if (isset($_GET['p2'])){
-        echo "this is : " . $_GET['p2'];
-    }
-
-  
+if (isset($_GET['p2'])){
+    echo "this is : " . $_GET['p2'];
+}  
 ?>
 
 
@@ -13,125 +10,156 @@
     <head lang="en">
         <meta charset="UTF-8">
         <title></title>
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/css/materialize.min.css">
+        <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
     </head>
     <body>
+        <!-- header -->
+        <nav class="purple darken-4">
+            <div class="nav-wrapper container">
+                <a href="#" class="brand-logo white-text">Jumo</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="settings.php"><i class="mdi-action-settings"></i></a></li>
+                    <li><a href="index.html">Logout</a></li>
+                </ul>
+            </div>
+        </nav>
+
+
         <div>
-
-
- <?php  
+            <?php  
             include ('database.php');
             include ('Player.php');
 
             ?>
 
-            <h1>Groups</h1>
+            <h4>Groups</h4>
+            <!-- <form>
+                <select class="browser-default">
+                    <option>test</option>
+
+                </select>
+            </form>
+
+            <div class="row">
+                <div class="col s4"><p></p></div>
+                <div class="col s4">
+                    <div class="selection center">
+                        <select class="browser-default" name="p2" required>
+                            <option value="" disabled selected>Please select a sport</option>
+                            <option value="sport" id="sport">Track and Field</option>
+                            <option value="2">Football</option>
+                            <option value="3">Tennis (Mens)</option>
+                        </select>
+                        <a class="waves-effect waves-light btn amber accent-3" href="month_view.html" id="submit">Go Huskies!</a>
+                    </div>
+                </div>
+            </div> -->
+
             <form action = "edit_students.php" method = "POST">
-                <select name="p2">
+                <select name="p2" class="browser-default">
                     <option></option>
-
-<?php 
-
-      foreach($connection->query("Select * from tblGROUP") as $row) {?>
-
-           <option value = <?php echo $row['GroupID'] ?> >
-                <span>
                     <?php 
-                        echo $row['GroupName'];
- 
-                    ?>
-                </span>
-                       </br>
-                      </option>
- 
-                     <?php }?>
+                    foreach($connection->query("Select * from tblGROUP") as $row) {?>
+                    <option value = <?php echo $row['GroupID'] ?> >
+                        <span>
+                            <?php 
+                            echo $row['GroupName'];
+                            ?>
+                        </span>
+                    </br>
+                </option>
+                <?php }?>
+                ?>
+            </select>
 
+
+            <!-- <a href="settings.php"><button id="createGroups">Create a Group</button></a> -->
+            <a href="settings.php" class="waves-effect waves-light amber accent-3 white-text btn">Create a Group</a>
+
+            <label for='formStudents[]'>Select the students:</label><br>
+            <select multiple="multiple" name="formStudents[]" class="browser-default">
+                <?php 
+
+                $playerArray = array();
+
+                foreach($connection->query("Select * from tblPLAYER") as $row) {
+                    $playerObject = new Player($row['PlayerName'], $row['PlayerID'], $row['PlayerContact']); 
+
+                    $playerArray[$row['PlayerName']] = $row['PlayerID'];
+
+                    ?>
+
+                    <option >
+                        <span>
+                            <?php 
+
+
+                            echo $playerObject ->getName();
+
+                            ?>
+                        </span>
+                    </br>
+                </option>
+
+                <?php }?>
+                <option>test</option>
+
+            </select>
+
+            <!-- <input name = "formSubmit" id = "formSubmit" type="submit" value="Test Submission"> -->
+            <button class="btn waves-effect waves-light amber accent-3 white-text" type="submit" id="formSubmit" name="formSubmit">Test Submission</button>
+
+
+        </form>
+        <a href="month_view.html" class="waves-effect waves-light amber accent-3 white-text btn">Submit (go to month)</a>
+
+
+
+        <?php
+
+        if(isset($_POST['formSubmit'])) 
+        {
+
+            echo '</br>' . $_POST['p2'] . '</br>';
+            $groupid = $_POST['p2'];
+
+            $aStudents = $_POST['formStudents'];
+
+            if(!isset($aStudents)) 
+            {
+                echo("<p>You didn't select any students!</p>\n");
+            } 
+            else
+            {
+
+                $statement = $connection->prepare ("INSERT INTO tblPLAYER_GROUP (PlayerID, GroupID) VALUES (:playerid, :groupid)"); 
+
+
+
+                $nStudents = count($aStudents);
+
+                echo("<p>You selected $nStudents countries: ");
+                for($i=0; $i < $nStudents; $i++)
+                {
+                    $player = $aStudents[$i];
+                    echo $aStudents[$i];
+                    $playerID = $playerArray[$player];
+
+                    $statement -> execute(array(':groupid' => $groupid, ':playerid' => $playerID)); 
+                }
+                echo("</p>");
+            }
+        }
 
 
         ?>
 
-                </select>
 
 
-            <a href="settings.html"><button id="createGroups">Create a Group</button></a>
-       
-                 <label for='formStudents[]'>Select the countries that you have visited:</label><br>
-                <select multiple="multiple" name="formStudents[]">
-                     <?php 
-
-                     $playerArray = array();
-    
-                     foreach($connection->query("Select * from tblPLAYER") as $row) {
-                       $playerObject = new Player($row['PlayerName'], $row['PlayerID'], $row['PlayerContact']); 
-
-                        $playerArray[$row['PlayerName']] = $row['PlayerID'];
-                        
-                       ?>
-
-           <option >
-                <span>
-                    <?php 
-                      
-                     
-                       echo $playerObject ->getName();
- 
-                    ?>
-                </span>
-                       </br>
-                      </option>
- 
-                     <?php }?>
- 
-                </select>
-
-            <input name = "formSubmit" id = "formSubmit" type="submit" value="Test Submission">
-            
-
-            </form>
-            <a href="month_view.html"><button id="submit">Submit</button></a>
-
-
-
-<?php
-
-if(isset($_POST['formSubmit'])) 
-{
-
-echo '</br>' . $_POST['p2'] . '</br>';
-$groupid = $_POST['p2'];
-
-  $aStudents = $_POST['formStudents'];
-   
-  if(!isset($aStudents)) 
-  {
-    echo("<p>You didn't select any students!</p>\n");
-  } 
-  else
-  {
-
- $statement = $connection->prepare ("INSERT INTO tblPLAYER_GROUP (PlayerID, GroupID) VALUES (:playerid, :groupid)"); 
-  
-
-
-    $nStudents = count($aStudents);
-     
-    echo("<p>You selected $nStudents countries: ");
-    for($i=0; $i < $nStudents; $i++)
-    {
-        $player = $aStudents[$i];
-        echo $aStudents[$i];
-         $playerID = $playerArray[$player];
-    
-        $statement -> execute(array(':groupid' => $groupid, ':playerid' => $playerID)); 
-    }
-    echo("</p>");
-  }
-}
-
- 
-?>
-
-
-
-        </div>
+    </div>
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/js/materialize.min.js"></script>
     </body>
 </html>
--- weekview.php
+++ weekview.php
@@ -2,381 +2,391 @@
 
 <!DOCTYPE html>
 <html>
-    <head lang="en">
-        <meta charset="UTF-8">
-        <title></title>
+	<head lang="en">
+		<meta charset="UTF-8">
+		<title></title>
+		<!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/css/materialize.min.css">
+        <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+	</head>
+	<body>
+		<!-- header -->
+        <nav class="purple darken-4">
+            <div class="nav-wrapper container">
+                <a href="#" class="brand-logo white-text">Jumo</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="settings.php"><i class="mdi-action-settings"></i></a></li>
+                    <li><a href="index.html">Logout</a></li>
+                </ul>
+            </div>
+        </nav>
 
-    </head>
-    <body>
-	<form action = "weekview.php" method = "POST">
-        <h2>Enter name of subgroup: </h2>
-        <select name="monthSelect">
-            <option></option>
+		<form action = "weekview.php" method = "POST">
+			<h2>Enter name of subgroup: </h2>
+			<select name="monthSelect" class="browser-default">
+				<option></option>
 
-			<?php 
-			include ('database.php');
-			 $datearray = array();
-			    foreach($connection->query("SELECT YEAR(dateName) AS 'year', MONTH(dateName) AS 'month' FROM tblDATE") as $row) {
-			 if ($row['year'] != '0' && strlen($row['year']) > 0) { 
+				<?php 
+				include ('database.php');
+				$datearray = array();
+				foreach($connection->query("SELECT YEAR(dateName) AS 'year', MONTH(dateName) AS 'month' FROM tblDATE") as $row) {
+					if ($row['year'] != '0' && strlen($row['year']) > 0) { 
 
-			 	$datearray[$row['year'].$row['month']] = $row['year'] . ' ' . $row['month'];
-			}
-			}
-			foreach ($datearray as $value) {
+						$datearray[$row['year'].$row['month']] = $row['year'] . ' ' . $row['month'];
+					}
+				}
+				foreach ($datearray as $value) {
 
-			 	?>
-			        <option>
-			            <span>
-			                <?php 
-			                	echo  $value;
-			                ?>
-			            </span>
-			            </br>
-			        </option>
-			 
-			    <?php }
+					?>
+					<option>
+						<span>
+							<?php 
+							echo  $value;
+							?>
+						</span>
+					</br>
+				</option>
+
+				<?php }
 
 				?>
 
-		 </select>
-	        
-
-	 	<select name="weekSelect">
-		 	 <option></option> </br>
-		     <option>1</option> </br>
-		      <option>2</option> </br>
-		      <option>3</option> </br>
-		      <option>4</option> </br>
-
-	    </select>
+			</select>
 
 
-	    <select name="groupSelect">
-                    <option></option>
-
-<?php 
-
-      foreach($connection->query("Select * from tblGROUP") as $row) {?>
-
-           <option value = <?php echo $row['GroupID'] ?>>
-                <span>
-                    <?php 
-                        echo $row['GroupName'];
- 
-                    ?>
-                </span>
-                       </br>
-                      </option>
- 
-                     <?php }?>
-        ?>
-
-                </select>
+			<select name="weekSelect" class="browser-default">
+				<option></option> </br>
+				<option>1</option> </br>
+				<option>2</option> </br>
+				<option>3</option> </br>
+				<option>4</option> </br>
+			</select>
 
 
+			<select name="groupSelect" class="browser-default">
+				<option></option>
 
-	    </br><input name = "formSubmit" type="submit" value="Select View">
-	 </form>
+				<?php 
+
+				foreach($connection->query("Select * from tblGROUP") as $row) {?>
+
+				<option value = <?php echo $row['GroupID'] ?>>
+					<span>
+						<?php 
+						echo $row['GroupName'];
+						?>
+					</span>
+				</br>
+				</option>
+
+				<?php }?>
+				?>
+
+			</select>
+		</br><input name = "formSubmit" type="submit" value="Select View">
+		</form>
 
 
 
-<table style="width:80%">
-  <tr>
-  	<td>Name</td>
-    <td>Sunday</td>
-    <td>Monday</td> 
-    <td>Tuesday</td>
-    <td>Wednesday</td>
-    <td>Thursday</td>
-    <td>Friday</td>
-    <td>Saturday</td>
-    <td>Total Time</td>
-  </tr>
+		<table style="width:80%">
+			<tr>
+				<td>Name</td>
+				<td>Sunday</td>
+				<td>Monday</td> 
+				<td>Tuesday</td>
+				<td>Wednesday</td>
+				<td>Thursday</td>
+				<td>Friday</td>
+				<td>Saturday</td>
+				<td>Total Time</td>
+			</tr>
 
 
 
-       <?php  
-include ('database.php');
+		<?php  
+		include ('database.php');
 
-if(isset($_POST['formSubmit'])) 
-{
+		if(isset($_POST['formSubmit'])) 
+		{
 
-$monthyear = $_POST['monthSelect'];
-$pieces = explode(" ", $monthyear);
-$year = $pieces[0];
-$month = $pieces[1];
-$nextmonth = $month+1;
-$week = $_POST['weekSelect'];
-$group = $_POST['groupSelect'];
+			$monthyear = $_POST['monthSelect'];
+			$pieces = explode(" ", $monthyear);
+			$year = $pieces[0];
+			$month = $pieces[1];
+			$nextmonth = $month+1;
+			$week = $_POST['weekSelect'];
+			$group = $_POST['groupSelect'];
 
-if ($week == 1) {
-	$week = '01';
-} else if ($week == 2) {
-	$week = '08';
-} else if ($week == 3){
-	$week = '15';
-} else {
-	$week = '22';
-}
+			if ($week == 1) {
+				$week = '01';
+			} else if ($week == 2) {
+				$week = '08';
+			} else if ($week == 3){
+				$week = '15';
+			} else {
+				$week = '22';
+			}
 
-$num_length = strlen((string)$month);
-if($num_length < 2) {
-    // Pass
-    $month = '0'.$month;
-} 
-
-
-$date = $year.$month.$week;
-$weekrange = week_range($date);
-$startday = $weekrange[0];
-$endday = $weekrange[1];
-$dayCount = 0;
-$weekHours = 0;
-$weekArray = array();
-$sumArray = array();
-
-	foreach($connection->query("Select StartTime, EndTime, PracticeTypeName, DateName, PlayerName
-		from tblPRACTICE p
-		join tblPRACTICE_TYPE pt
-		on p.PracticeTypeID = pt.PracticeTypeID
-		join tblDATE d
-		on p.DateID = d.DateID
-		join tblPLAYER_PRACTICE pp
-		on pp.PracticeID = p.PracticeID
-		join tblPLAYER pl
-		on pp.PlayerID = pl.PlayerID
-		WHERE DATE(d.DateName) BETWEEN '$startday' AND '$endday' AND (p.GroupID = $group)
-		ORDER BY DateName ASC") as $row) { 
+			$num_length = strlen((string)$month);
+			if($num_length < 2) {
+	// Pass
+				$month = '0'.$month;
+			} 
 
 
-		$name = $row['PlayerName'];
+			$date = $year.$month.$week;
+			$weekrange = week_range($date);
+			$startday = $weekrange[0];
+			$endday = $weekrange[1];
+			$dayCount = 0;
+			$weekHours = 0;
+			$weekArray = array();
+			$sumArray = array();
 
-		$timestamp = strtotime($row['DateName']);
-		$dayOfWeek = date("w", $timestamp);
-		$startTime = strtotime($row['StartTime']);
-		$endTime = strtotime($row['EndTime']);
+			foreach($connection->query("Select StartTime, EndTime, PracticeTypeName, DateName, PlayerName
+				from tblPRACTICE p
+				join tblPRACTICE_TYPE pt
+				on p.PracticeTypeID = pt.PracticeTypeID
+				join tblDATE d
+				on p.DateID = d.DateID
+				join tblPLAYER_PRACTICE pp
+				on pp.PracticeID = p.PracticeID
+				join tblPLAYER pl
+				on pp.PlayerID = pl.PlayerID
+				WHERE DATE(d.DateName) BETWEEN '$startday' AND '$endday' AND (p.GroupID = $group)
+				ORDER BY DateName ASC") as $row) { 
 
 
-		if(!array_key_exists($name, $weekArray)){
-			$weekArray[$name][$dayOfWeek] = array();
-		} 
+				$name = $row['PlayerName'];
+
+			$timestamp = strtotime($row['DateName']);
+			$dayOfWeek = date("w", $timestamp);
+			$startTime = strtotime($row['StartTime']);
+			$endTime = strtotime($row['EndTime']);
 
 
-		if(!array_key_exists($name, $sumArray)){
-			$sumArray[$name][$dayOfWeek] = array();
-		} 
+			if(!array_key_exists($name, $weekArray)){
+				$weekArray[$name][$dayOfWeek] = array();
+			} 
 
-		if (!array_key_exists($dayOfWeek, $weekArray[$name])) {
-			$dayInput = $startTime . ' ' . $endTime;
-			$weekArray[$name][$dayOfWeek][0] = $dayInput;
 
-		} else {
-			$size = sizeof($weekArray[$name][$dayOfWeek]);
-			$weekArray[$name][$dayOfWeek][$size] = $startTime . ' ' . $endTime;
-		} 
+			if(!array_key_exists($name, $sumArray)){
+				$sumArray[$name][$dayOfWeek] = array();
+			} 
 
-		$diffValue = $endTime - $startTime;
-		if (!array_key_exists($dayOfWeek, $sumArray[$name]) || count($sumArray[$name][$dayOfWeek]) == 0) {			
-			$sumArray[$name][$dayOfWeek][0] = $diffValue;
-		} else {
+			if (!array_key_exists($dayOfWeek, $weekArray[$name])) {
+				$dayInput = $startTime . ' ' . $endTime;
+				$weekArray[$name][$dayOfWeek][0] = $dayInput;
 
-			$sumArray[$name][$dayOfWeek][0] += $diffValue;
-		} 
+			} else {
+				$size = sizeof($weekArray[$name][$dayOfWeek]);
+				$weekArray[$name][$dayOfWeek][$size] = $startTime . ' ' . $endTime;
+			} 
 
- } ?>
-    <?php 
-    $dayTracker = array();
-    $dayTracker[0] = -1;
-    $hourLimit = 72000;
-    $dayLimit = 14400;
-    $dayHours = array();
-    $daysPracticed = 0;
+			$diffValue = $endTime - $startTime;
+			if (!array_key_exists($dayOfWeek, $sumArray[$name]) || count($sumArray[$name][$dayOfWeek]) == 0) {			
+				$sumArray[$name][$dayOfWeek][0] = $diffValue;
+			} else {
 
-    foreach ($weekArray as $key => $value) {
-    	$firsttime = true;?>
-		<tr>
-    	<td><?php echo $key ?></td>
-    	<?php 
-    		foreach ($value as $dayKey => $timesArray) { 
-    			
-    			if ($firsttime == true) {
-    				for ($f = 0; $f < ($dayKey); $f++) { ?>
-    				<td>No Practice</td>
-    			<?php }
-    			$firsttime = false; 
-    			}
-    			
-    			?>
-    			
-    			<?php
-    			if($dayTracker[0] != -1 && ($dayKey - $dayTracker[0]) > 1) {
-    				for ($j= 0; $j < ($dayKey-$dayTracker[0])-1; $j++) { ?>
-    					<td>No Practice</td>
+				$sumArray[$name][$dayOfWeek][0] += $diffValue;
+			} 
 
-    			<?php }
+		} ?>
+		<?php 
+		$dayTracker = array();
+		$dayTracker[0] = -1;
+		$hourLimit = 72000;
+		$dayLimit = 14400;
+		$dayHours = array();
+		$daysPracticed = 0;
 
-    			}
+		foreach ($weekArray as $key => $value) {
+			$firsttime = true;?>
+			<tr>
+				<td><?php echo $key ?></td>
+				<?php 
+				foreach ($value as $dayKey => $timesArray) { 
+
+					if ($firsttime == true) {
+						for ($f = 0; $f < ($dayKey); $f++) { ?>
+						<td>No Practice</td>
+						<?php }
+						$firsttime = false; 
+					}
+
+					?>
+
+					<?php
+					if($dayTracker[0] != -1 && ($dayKey - $dayTracker[0]) > 1) {
+						for ($j= 0; $j < ($dayKey-$dayTracker[0])-1; $j++) { ?>
+						<td>No Practice</td>
+
+						<?php }
+
+					}
 					$dayTracker[0] = $dayKey;
 					if($dayTracker[0] != -1) {
 						if(!array_key_exists($dayTracker[0], $dayHours)){
 							$dayHours[$dayTracker[0]] = 0;
 						} 
 					}
-					
-				?>
-    			<td>
-    			<?php 
-    				foreach ($timesArray as $timeKey => $timeValue) {
-    					$timePieces = explode(' ', $timeValue);
-    					$startPiece = $timePieces[0];
-    					$endPiece = $timePieces[1];
+
+					?>
+					<td>
+						<?php 
+						foreach ($timesArray as $timeKey => $timeValue) {
+							$timePieces = explode(' ', $timeValue);
+							$startPiece = $timePieces[0];
+							$endPiece = $timePieces[1];
 
 
-    					$difference = $sumArray[$key][$dayKey][0];
-						$dayDifference = $difference;
-						$diffDisplay = '';
-						$hours = '';
-						$minutes = '';
-						$seconds = '';
-						
-						if ($dayTracker[0] != -1) {
-							$weekDay = $dayTracker[0];
+							$difference = $sumArray[$key][$dayKey][0];
+							$dayDifference = $difference;
+							$diffDisplay = '';
+							$hours = '';
+							$minutes = '';
+							$seconds = '';
 
-							$dayHours[$weekDay] = $dayHours[$weekDay] + $difference; 
+							if ($dayTracker[0] != -1) {
+								$weekDay = $dayTracker[0];
 
+								$dayHours[$weekDay] = $dayHours[$weekDay] + $difference; 
+
+							}
+
+							if($difference > 3600){
+
+								$hours = abs($difference/3600 %24);
+								$difference = $difference - ($hours * 3600);
+								$diffDisplay .= $hours . ' hrs ';
+							} 
+
+							if($difference >= 60){
+								$minutes = abs($difference/60%60);
+								$difference = $difference - ($minutes * 60);
+								$diffDisplay .= $minutes . ' min ';
+							}
+
+							if ($difference <60) {
+								$seconds = $difference;
+								$diffDisplay .= $seconds . ' s ';
+							} 	
+
+							if ($startPiece < 18000 || $endPiece < 18000) {
+								echo 'Violation: Not supposed to be practicing at this time' . '</br>';
+							}
+
+							echo timeFormat($startPiece) . ' to ' . timeFormat($endPiece) .'</br>';
 						}
-
-						 if($difference > 3600){
-
-							$hours = abs($difference/3600 %24);
-							$difference = $difference - ($hours * 3600);
-							$diffDisplay .= $hours . ' hrs ';
-						} 
-						
-						if($difference >= 60){
-							$minutes = abs($difference/60%60);
-							$difference = $difference - ($minutes * 60);
-							$diffDisplay .= $minutes . ' min ';
+						$weekHours += $dayDifference;
+						$daysPracticed = count($sumArray[$key]);
+						if ($dayDifference > $dayLimit){
+							echo '<font color="red">' .  $diffDisplay . '</font>' . '</br>';
+						} else {
+							echo $diffDisplay . '</br>';
 						}
+						?> 
+					</td> 
 
-						if ($difference <60) {
-						 	$seconds = $difference;
-						 	$diffDisplay .= $seconds . ' s ';
-						 } 	
+					<?php }
 
-						 if ($startPiece < 18000 || $endPiece < 18000) {
-						 	echo 'Violation: Not supposed to be practicing at this time' . '</br>';
-						 }
+					if($dayTracker[0] != -1 && (7 - $dayTracker[0]) > 1) {
+						for ($j= 0; $j < (7-$dayTracker[0])-1; $j++) { ?>
+						<td>No Practice</td>
+						<?php }
+					}
+					?>
 
-    					echo timeFormat($startPiece) . ' to ' . timeFormat($endPiece) .'</br>';
-    				}
-    				$weekHours += $dayDifference;
-    				$daysPracticed = count($sumArray[$key]);
-    				if ($dayDifference > $dayLimit){
-    					echo '<font color="red">' .  $diffDisplay . '</font>' . '</br>';
-    				} else {
-    					echo $diffDisplay . '</br>';
-    				}
-    			?> 
-    			</td> 
+					<td><?php 
+					if ($daysPracticed > 3) {
+						echo 'Violaion: No Break Days' . '</br>';
+					}
 
-    		<?php }
+					if ($weekHours > $hourLimit){
+						echo '<font color="red">' .sumFormat($weekHours) . '</font>';
+					} else {
+						echo  sumFormat($weekHours); 
+					} ?>
 
-    		if($dayTracker[0] != -1 && (7 - $dayTracker[0]) > 1) {
-    				for ($j= 0; $j < (7-$dayTracker[0])-1; $j++) { ?>
-    					<td>No Practice</td>
-    			<?php }
-    		}
-    	?>
+				</td>
 
-    	<td><?php 
-    		if ($daysPracticed > 3) {
-    			echo 'Violaion: No Break Days' . '</br>';
-    		}
+			</tr>
+			<?php }?> 
 
-    		if ($weekHours > $hourLimit){
-				echo '<font color="red">' .sumFormat($weekHours) . '</font>';
-    		} else {
-    			echo  sumFormat($weekHours); 
-    		} ?>
-
-    	</td>
-
-    	</tr>
-    <?php }?> 
-  
 		</table>
 
 
-	<?php 
+		<?php 
 
 	}
 
-?>
-    </body>
+	?>
+		<!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/js/materialize.min.js"></script>
+	</body>
 </html>
 
 
 
 <?php 
 function week_range($date) {
-  $ts = strtotime($date);
-  $start = (date('w', $ts) == 0) ? $ts : strtotime('last sunday', $ts);
-  return array(date('Y.m.d', $start), 
-  	date('Y.m.d', strtotime('next saturday', $start)));
+	$ts = strtotime($date);
+	$start = (date('w', $ts) == 0) ? $ts : strtotime('last sunday', $ts);
+	return array(date('Y.m.d', $start), 
+		date('Y.m.d', strtotime('next saturday', $start)));
 }
 
 
 function sumFormat($time) {
- 		$timeDisplay = '';
- 		$minPhrase = ' min ';
-		 if($time > 3600){
-			$hours = abs($time/3600% 24);
-			$time = $time - ($hours * 3600);
+	$timeDisplay = '';
+	$minPhrase = ' min ';
+	if($time > 3600){
+		$hours = abs($time/3600% 24);
+		$time = $time - ($hours * 3600);
 
-			$timeDisplay .= $hours . ' hrs ';
-		} 
-		
-		if($time >= 60){
-			$minutes = abs($time/60 % 60);
-			$time = $time - ($minutes * 60);
-			$timeDisplay .= $minutes . ' min ';
-		}
+		$timeDisplay .= $hours . ' hrs ';
+	} 
 
-		if ($time <60) {
-		 	$seconds = $time;
-		 	$timeDisplay .= $seconds . ' s ';
-		 }
-		 return $timeDisplay;
+	if($time >= 60){
+		$minutes = abs($time/60 % 60);
+		$time = $time - ($minutes * 60);
+		$timeDisplay .= $minutes . ' min ';
+	}
+
+	if ($time <60) {
+		$seconds = $time;
+		$timeDisplay .= $seconds . ' s ';
+	}
+	return $timeDisplay;
 }
 
 function timeFormat($time) {
- 		$timeDisplay = '';
- 		$minPhrase = ' min ';
-		 if($time > 3600){
-			$hours = abs($time/3600% 24);
-			$hours = $hours + 2;
-			$time = $time - ($hours * 3600);
-			if ($hours > 24) {
-				$hours = $hours -24;
-			} else if ($hours > 12){
-				$hours  = $hours-12;
-			}
-
-			$timeDisplay .= $hours . ':';
-			$minPhrase = ' ';
-		} 
-		
-		if($time >= 60){
-			$minutes = abs($time/60 % 60);
-			$time = $time - ($minutes * 60);
-			$timeDisplay .= $minutes . $minPhrase;
+	$timeDisplay = '';
+	$minPhrase = ' min ';
+	if($time > 3600){
+		$hours = abs($time/3600% 24);
+		$hours = $hours + 2;
+		$time = $time - ($hours * 3600);
+		if ($hours > 24) {
+			$hours = $hours -24;
+		} else if ($hours > 12){
+			$hours  = $hours-12;
 		}
 
-		if ($time <60) {
-		 	$seconds = $time;
-		 	$timeDisplay .= $seconds . ' s ';
-		 }
-		 return $timeDisplay;
+		$timeDisplay .= $hours . ':';
+		$minPhrase = ' ';
+	} 
+
+	if($time >= 60){
+		$minutes = abs($time/60 % 60);
+		$time = $time - ($minutes * 60);
+		$timeDisplay .= $minutes . $minPhrase;
+	}
+
+	if ($time <60) {
+		$seconds = $time;
+		$timeDisplay .= $seconds . ' s ';
+	}
+	return $timeDisplay;
 }
 ?>
