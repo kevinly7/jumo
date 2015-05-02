e49a45c6e6731c3ff690215edf2f6c1c4dfad93a
Tuvshin Tulga <mbmtuvshin@yahoo.com>
2015-05-02 14:59:31 -0700

    Styled coach view, messed with line spacing for the view pages, and moved the nav buttons to the outside

--- violations.php
+++ violations.php
@@ -10,7 +10,7 @@
     <body>
     	<!-- header -->
         <nav class="purple darken-4">
-            <div class="nav-wrapper container">
+            <div class="nav-wrapper">
                 <a href="weekview.php" class="brand-logo white-text">Jumo</a>
                 <ul id="nav-mobile" class="right hide-on-med-and-down">
                     <li><a href="settings.php"><i class="mdi-action-settings"></i></a></li>
@@ -24,7 +24,10 @@
 		<div class="row">
 			<div class="col s3">
 				<form action = "violations.php" method = "POST">
-			        <!-- <br> -->
+			        <br>
+					<br>
+					<br>
+					
 			        <h6><b>Filters</b></h6>	
 			        <br>			        
 			        <select name="monthSelect" class="browser-default">
@@ -78,7 +81,7 @@
 			</div>
 
 			<div class="col s9">
-				<table class="bordered striped">
+				<table class="bordered hoverable">
 					<thead>
 						<tr>
 							<th data-field="name">Name</th>
@@ -245,8 +248,11 @@
 		</div>
 		<?php 
 		}?>
-		<!-- Compiled and minified JavaScript -->
+		<!--  Scripts-->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/js/materialize.min.js"></script>
+        <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script src="js/materialize.js"></script>
+        <script src="js/init.js"></script>
     </body>
 </html>
 
--- weekview.php
+++ weekview.php
@@ -10,7 +10,7 @@
     <body>
     	<!-- header -->
     	<nav class="purple darken-4">
-            <div class="nav-wrapper container">
+            <div class="nav-wrapper">
                 <a href="weekview.php" class="brand-logo white-text">Jumo</a>
                 <ul id="nav-mobile" class="right hide-on-med-and-down">
                     <li><a href="settings.php"><i class="mdi-action-settings"></i></a></li>
@@ -19,9 +19,14 @@
             </div>
         </nav>
 
+        <h4 class="center">Week View</h4>
+
     	<div class="row">
 			<div class="col s3">	
 				<form action = "weekview.php" method = "POST"> 
+					<br>
+					<br>
+					<br>
 					<br>
 			        <h6><b>Filters</b></h6>	
 			        <br>
@@ -141,7 +146,7 @@
 						$weekrange = week_range($date);
 						$startday = $weekrange[0];
 						$endday = $weekrange[1]; ?>
-						<p class = "viewTitle"> <?php echo "Checking practices for " . $groupArray[$group] . ' on ' . $startday . ' to ' . $endday; ?> </p>
+						<p class = "viewTitle"> <?php echo "Checking practices from " . $groupArray[$group] . '' . $startday . ' to ' . $endday; ?> </p>
 						<?php $dayCount = 0;
 						$weekHours = 0;
 						$weekArray = array();
@@ -307,8 +312,11 @@
 	    </div>
 		<?php 
 		}?>
-		<!-- Compiled and minified JavaScript -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/js/materialize.min.js"></script>
+		<!--  Scripts-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/js/materialize.min.js"></script>
+        <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script src="js/materialize.js"></script>
+        <script src="js/init.js"></script>
     </body>
 </html>
 
