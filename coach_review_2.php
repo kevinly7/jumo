bd6e3e1079d5935c8b62a105cc58d9aa4b61bda8
Information School <ischoolit@uw.edu>
2015-05-02 19:10:15 -0700

    more changes to review page

--- coach_review_2.php
+++ coach_review_2.php
@@ -212,7 +212,8 @@
 									<input type="hidden" name=dateID'.$practiceCounter.' value='.$dateID.'>
 									<input type="hidden" name=playerID'.$practiceCounter.' value='.$playerID.'>
 									<input type="hidden" name=practiceID'.$practiceCounter.' value='.$practiceID.'>'.
-									'<input type="checkbox" class="filled-in browser-default" name=edit'.$practiceCounter.' value="edit">'.$practiceCounter.'</br>'.
+									'<input type="checkbox" class="filled-in browser-default" name=edit'.$practiceCounter.' value="edit">Edit</br>'.
+									'<input type="checkbox" class="filled-in browser-default" name=delete'.$practiceCounter.' value="edit">Delete</br>'.
 									'<input type = "time" name=startTime'.$practiceCounter.' value=' . timeFormat($startTime) . '> to <input type="time" name=endTime'.$practiceCounter.' value=' . timeFormat($endTime) . '> </br>';
 									$weekArray[$name][$dayOfWeek][0] = $dayInput;
 
@@ -223,8 +224,8 @@
 									<input type="hidden" name=dateID'.$practiceCounter.' value='.$dateID.'>
 									<input type="hidden" name=playerID'.$practiceCounter.' value='.$playerID.'>
 									<input type="hidden" name=practiceID'.$practiceCounter.' value='.$practiceID.'>'.
-									'<input type="checkbox" class="filled-in browser-default" name=edit'.$practiceCounter.' value="edit">'.$practiceCounter.' edit'.'</br>'.
-									'<input type="checkbox" class="filled-in browser-default" name=delete'.$practiceCounter.' value="edit">'.$practiceCounter.' delete'.'</br>'.
+									'<input type="checkbox" class="filled-in browser-default" name=edit'.$practiceCounter.' value="edit">Edit</br>'.
+									'<input type="checkbox" class="filled-in browser-default" name=delete'.$practiceCounter.' value="edit">Delete</br>'.
 									'<input type = "time" name=startTime'.$practiceCounter.' value=' . timeFormat($startTime) . '> to <input type="time" name=endTime'.$practiceCounter.' value=' . timeFormat($endTime) . '> </br>';
 								} 
 
@@ -320,7 +321,7 @@ function timeFormat($time) {
 
 			$hours = abs($time/3600% 24);
 			$hours = $hours + 2;
-			echo $hours." ";
+			//echo $hours." ";
 			$time = $time - ($hours * 3600);
 			if ($hours > 23) {
 				$hours = $hours -24;
