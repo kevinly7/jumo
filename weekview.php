3255dcae951826c4c027be80c2a6348b815410ec
Howard Lin <hhowardlin@msn.com>
2015-05-06 10:48:14 -0700

    correct version

--- weekview.php
+++ weekview.php
@@ -120,6 +120,7 @@
 					include ('database.php');
 					if(isset($_POST['formSubmit'])) 
 					{
+						$monthyear = $_POST['monthSelect'];
 						$pieces = explode(" ", $monthyear);
 						$year = $pieces[0];
 						$month3 = $pieces[1];
