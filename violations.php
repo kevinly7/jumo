51bc757ebf403969a26cfa72b137dfa69213e666
Information School <ischoolit@uw.edu>
2015-05-30 12:40:23 -0700

    checks if there are violations

--- violations.php
+++ violations.php
@@ -232,121 +232,145 @@ function printWeek($year, $month, $week, $group, $default){
 		$weekArray = array();
 		$sumArray = array();
 						?>
-					<p class = "viewTitle"> <?php echo "Checking violations from " . $startday . ' to ' . $endday; ?> </p>
+		<p class = "viewTitle"> <?php echo "Checking violations from " . $startday . ' to ' . $endday; ?> </p>
 	<?php
-					foreach($connection->query("Select StartTime, EndTime, PracticeTypeName, DateName, PlayerName
-						from tblPRACTICE p
-						join tblPRACTICE_TYPE pt
-						on p.PracticeTypeID = pt.PracticeTypeID
-						join tblDATE d
-						on p.DateID = d.DateID
-						join tblPLAYER_PRACTICE pp
-						on pp.PracticeID = p.PracticeID
-						join tblPLAYER pl
-						on pp.PlayerID = pl.PlayerID
-						WHERE DATE(d.DateName) BETWEEN '$startday' AND '$endday'
-						ORDER BY DateName ASC") as $row) { 
-						$name = $row['PlayerName'];
-						$timestamp = strtotime($row['DateName']);
-						$dayOfWeek = date("w", $timestamp);
-						$startTime = strtotime($row['StartTime']);
-						$endTime = strtotime($row['EndTime']);
-						if(!array_key_exists($name, $weekArray)){
-							$weekArray[$name][$dayOfWeek . ' ' . $row['DateName']] = array();
-						} 
-						if(!array_key_exists($name, $sumArray)){
-							$sumArray[$name][$dayOfWeek . ' ' . $row['DateName']] = array();
-						} 
-						if (!array_key_exists($dayOfWeek, $weekArray[$name])) {
-							$dayInput = $startTime . ' ' . $endTime;
-							$weekArray[$name][$dayOfWeek . ' ' . $row['DateName']][0] = $dayInput;
-						} else {
-							$size = sizeof($weekArray[$name][$dayOfWeek]);
-							$weekArray[$name][$dayOfWeek][$size] = $startTime . ' ' . $endTime;
-						} 
-						$diffValue = $endTime - $startTime;
-						if (!array_key_exists($dayOfWeek, $sumArray[$name]) || count($sumArray[$name][$dayOfWeek]) == 0) {			
-							$sumArray[$name][$dayOfWeek][0] = $diffValue;
-						} else {
-							$sumArray[$name][$dayOfWeek][0] += $diffValue;
-						} 
-					} ?>
+				$violationquery = $connection->query("Select StartTime, EndTime, PracticeTypeName, DateName, PlayerName
+					from tblPRACTICE p
+					join tblPRACTICE_TYPE pt
+					on p.PracticeTypeID = pt.PracticeTypeID
+					join tblDATE d
+					on p.DateID = d.DateID
+					join tblPLAYER_PRACTICE pp
+					on pp.PracticeID = p.PracticeID
+					join tblPLAYER pl
+					on pp.PlayerID = pl.PlayerID
+					WHERE DATE(d.DateName) BETWEEN '$startday' AND '$endday'
+					ORDER BY DateName ASC");
 
+				if($violationquery->rowCount() < 1)
+				{ ?>
+
+				<tbody>
+					<tr>
+						<td>
+					<?php
+
+				    // row exists. do whatever you want to do.
+				    echo "There are no violations for this week"; ?>
+				    </td>
+				</tr>
+			</tbody>
+			<?php
+				} else {
+
+
+
+
+				foreach($violationquery as $row) { 
+					$name = $row['PlayerName'];
+					$timestamp = strtotime($row['DateName']);
+					$dayOfWeek = date("w", $timestamp);
+					$startTime = strtotime($row['StartTime']);
+					$endTime = strtotime($row['EndTime']);
+					if(!array_key_exists($name, $weekArray)){
+						$weekArray[$name][$dayOfWeek . ' ' . $row['DateName']] = array();
+					} 
+					if(!array_key_exists($name, $sumArray)){
+						$sumArray[$name][$dayOfWeek . ' ' . $row['DateName']] = array();
+					} 
+					if (!array_key_exists($dayOfWeek, $weekArray[$name])) {
+						$dayInput = $startTime . ' ' . $endTime;
+						$weekArray[$name][$dayOfWeek . ' ' . $row['DateName']][0] = $dayInput;
+					} else {
+						$size = sizeof($weekArray[$name][$dayOfWeek]);
+						$weekArray[$name][$dayOfWeek][$size] = $startTime . ' ' . $endTime;
+					} 
+					$diffValue = $endTime - $startTime;
+					if (!array_key_exists($dayOfWeek, $sumArray[$name]) || count($sumArray[$name][$dayOfWeek]) == 0) {			
+						$sumArray[$name][$dayOfWeek][0] = $diffValue;
+					} else {
+						$sumArray[$name][$dayOfWeek][0] += $diffValue;
+					} 
+				} ?>
+
+			    <?php 
+			    $hourLimit = 72000;
+			    $dayLimit = 14400;
+			    $dayHours = array();
+			    $daysPracticed = 0; 
+			    $dayTracker = array();
+			    $dayTracker[0] = -1;
+
+			    foreach ($weekArray as $key => $value) { $weekhours = 0;?>
+
+			    	<tbody>
+						<tr>
+							<td><?php echo $key ?></td>
+							<td>
+					    		<?php 
+					    		foreach ($value as $dayK => $timesArray) {   
+					    			$dayPieces = explode(" ", $dayK);
+									$dayKey = $dayPieces[0];
+									$dateKey = $dayPieces[1];
+									?>
+					    			
+					    			<?php  
+					    				foreach ($timesArray as $timeKey => $timeValue) {
+					    					$timePieces = explode(' ', $timeValue);
+					    					$startPiece = $timePieces[0];
+					    					$endPiece = $timePieces[1];
+					    					$difference = $sumArray[$key][$dayKey][0];
+
+					    					$dayTracker[0] = $dayKey;
+											if($dayTracker[0] != -1) {
+												if(!array_key_exists($dayTracker[0], $dayHours)){
+													$dayHours[$dayTracker[0]] = 0;
+												} 
+											}
+											
+											if ($dayTracker[0] != -1) {
+												$weekDay = $dayTracker[0];
+												$dayHours[$weekDay] = $dayHours[$weekDay] + $difference; 
+											}
+
+											$correctStart = date('h:i:s A', $startPiece);
+											$endStart = date('h:i:s A', $endPiece);
+											$practiceStart = strtotime('12:00:00 AM');
+											$practiceEnd = strtotime('05:00:00 AM');
+											
+											if (($startPiece > $practiceStart && $startPiece < $practiceEnd) ||
+												($endPiece > $practiceStart && $endPiece < $practiceEnd)) {
+												echo 'Not supposed to be practicing at the time ' . $correctStart . ' to ' . $endStart . ' on ' . $dateKey .'</br>';
+											}
+					    				}
+					    				$weekHours += $difference;
+					    				$daysPracticed = count($sumArray[$key]);
+					    				if ($difference > $dayLimit){ 
+					    				 echo 'Over 4 hours on ' . $dateKey . '</br>';
+					    				} 
+					    			 ?> 
+					    			 
+					    		<?php }
+					    		?>
+
+					    		<?php 
+					    		if ($daysPracticed > 3) { 
+								   echo 'No Break Days this week' . '</br>';
+					    		}
+
+					    		if ($weekHours > $hourLimit){ 
+					    			echo 'Over the 20 hour weekly limit' ;
+					    		} 
+					    		?> 
+								</td>
+					    	</tr>
+					    </tbody>
 				    <?php 
-				    $hourLimit = 72000;
-				    $dayLimit = 14400;
-				    $dayHours = array();
-				    $daysPracticed = 0; 
-				    $dayTracker = array();
-				    $dayTracker[0] = -1;
+				}
 
-				    foreach ($weekArray as $key => $value) { $weekhours = 0;?>
-
-				    	<tbody>
-							<tr>
-								<td><?php echo $key ?></td>
-								<td>
-						    		<?php 
-						    		foreach ($value as $dayK => $timesArray) {   
-						    			$dayPieces = explode(" ", $dayK);
-										$dayKey = $dayPieces[0];
-										$dateKey = $dayPieces[1];
-										?>
-						    			
-						    			<?php  
-						    				foreach ($timesArray as $timeKey => $timeValue) {
-						    					$timePieces = explode(' ', $timeValue);
-						    					$startPiece = $timePieces[0];
-						    					$endPiece = $timePieces[1];
-						    					$difference = $sumArray[$key][$dayKey][0];
-
-						    					$dayTracker[0] = $dayKey;
-												if($dayTracker[0] != -1) {
-													if(!array_key_exists($dayTracker[0], $dayHours)){
-														$dayHours[$dayTracker[0]] = 0;
-													} 
-												}
-												
-												if ($dayTracker[0] != -1) {
-													$weekDay = $dayTracker[0];
-													$dayHours[$weekDay] = $dayHours[$weekDay] + $difference; 
-												}
-
-												$correctStart = date('h:i:s A', $startPiece);
-												$endStart = date('h:i:s A', $endPiece);
-												$practiceStart = strtotime('12:00:00 AM');
-												$practiceEnd = strtotime('05:00:00 AM');
-												
-												if (($startPiece > $practiceStart && $startPiece < $practiceEnd) ||
-													($endPiece > $practiceStart && $endPiece < $practiceEnd)) {
-													echo 'Not supposed to be practicing at the time ' . $correctStart . ' to ' . $endStart . ' on ' . $dateKey .'</br>';
-												}
-						    				}
-						    				$weekHours += $difference;
-						    				$daysPracticed = count($sumArray[$key]);
-						    				if ($difference > $dayLimit){ 
-						    				 echo 'Over 4 hours on ' . $dateKey . '</br>';
-						    				} 
-						    			 ?> 
-						    			 
-						    		<?php }
-						    		?>
-
-						    		<?php 
-						    		if ($daysPracticed > 3) { 
-									   echo 'No Break Days this week' . '</br>';
-						    		}
-
-						    		if ($weekHours > $hourLimit){ 
-						    			echo 'Over the 20 hour weekly limit' ;
-						    		} 
-						    		?> 
-									</td>
-						    	</tr>
-						    </tbody>
-					    <?php }?>   
-				</table>
-				<br>
+				}?>   
+			</table>
+			<br>
 				<!-- <button class="btn waves-effect waves-light amber accent-3 white-text right" type="submit" id="submitGroup" name="formSubmit">Submit</button> -->
 			</div>
 		</div>
