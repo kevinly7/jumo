3781e933782848c664a028241ba32b520d5e89b8
Information School <ischoolit@uw.edu>
2015-06-02 22:52:59 -0700

    delete players

--- addPlayer.php
+++ addPlayer.php
@@ -1,7 +1,17 @@
 <?php
+	include("database.php");
 	if(isset($_POST["formSubmit"])) {
 		$playerName = $_POST["PlayerName"];
 		$email = $_POST["email"];
+		$statement2 = $connection->prepare("SELECT * FROM tblPLAYER WHERE  PlayerName = :player");
+		$statement2 -> execute(array(':player' => $playerName));
+		$result = $statement2->fetchAll();
+		if(!empty($result)) {
+			echo "Player already exists in system";
+		} else {
+			$statement = $connection->prepare ("INSERT INTO tblPLAYER(PlayerName, PlayerContact) VALUES (:player, :contact)");
+			$statement -> execute(array(':player' =>$playerName, ':contact' =>$email));
+		}
 		echo $playerName . " " . $email . " " . $password;
 	}
 
---
+++ deletePlayers.php
@@ -0,0 +1,31 @@
+<html>
+	<head>
+	</head>
+	<body>
+		<?php
+			include ('database.php');
+            include ('Player.php');
+        ?>
+        <form action="delete.php" action="POST">
+			<select multiple="multiple" name="formStudents[]" class="browser-default student-select ">
+
+        <?php   
+    		foreach($connection->query("Select * from tblPLAYER")  as $row) {
+            $playerObject = new Player($row['PlayerName'], $row['PlayerID'], $row['PlayerContact']); 
+			$playerArray[$row['PlayerName']] = $row['PlayerID'];
+		?>
+        
+	        <option class="student" value = <?php echo $row['PlayerID'] ?> >
+	            <span class="student">
+	                <?php 
+	                echo $playerObject ->getName();
+	                ?>
+	            </span>
+	        </option>
+
+            <?php }?>
+        </select>
+    	</form>
+        
+	</body>
+</html>
