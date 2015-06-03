1a7f07838640f826866c9ef47f904ca8698f1202
kyang126 <kevinjyang126@gmail.com>
2015-06-02 17:22:14 -0700

    Create groupStudents.php

---
+++ groupStudents.php
@@ -0,0 +1,16 @@
+<?php
+echo "YESSSS";
+include("database.php");
+if (isset($_POST['action'])) {
+	$group = $_POST['action'];
+	echo "does this work: " . $group;
+	function showStudents(){ 
+
+		return echo "hello";
+
+		?>
+		
+	<?php }
+}
+
+?>
