1d5f3fc9115856b02e42a657fcd6bf3877f8b59c
Information School <ischoolit@uw.edu>
2015-06-03 11:19:52 -0700

    delete and add functionality

--- addPlayer.php
+++ addPlayer.php
@@ -1,20 +1,45 @@
-<?php
-	include("database.php");
-	if(isset($_POST["formSubmit"])) {
-		$playerName = $_POST["PlayerName"];
-		$email = $_POST["email"];
-		$statement2 = $connection->prepare("SELECT * FROM tblPLAYER WHERE  PlayerName = :player");
-		$statement2 -> execute(array(':player' => $playerName));
-		$result = $statement2->fetchAll();
-		if(!empty($result)) {
-			echo "Player already exists in system";
-		} else {
-			$statement = $connection->prepare ("INSERT INTO tblPLAYER(PlayerName, PlayerContact) VALUES (:player, :contact)");
-			$statement -> execute(array(':player' =>$playerName, ':contact' =>$email));
-		}
-		echo $playerName . " " . $email . " " . $password;
-	}
+<html>
+	<header>
+	</header>
+
+	<body>
+		<form action="addPlayerBack.php" onSubmit = "validateForm()" method="POST">
+			<input type="text" name="PlayerName">
+			<input type="email" name="email">
+			
+			<?php  
+	            include ('database.php');
+            ?>
+            <select name="group" class="browser-default">
+                <option value="" disabled selected>Please select a group</option>
+                <?php 
+                foreach($connection->query("Select * from tblGROUP ORDER BY GroupName ASC") as $row) {?>
+                <option value = <?php echo $row['GroupID'] ?> >
+                    <span>
+                        <?php 
+                        echo $row['GroupName'];
+                        ?>
+                    </span>
+                    </br>
+                </option>
+                <?php }?>
+                ?>
+            </select>
+            <input type="submit" value="Submit" name="formSubmit">     
+		</form>
+		<script>
+			function validateForm() {
+			    var inputs = document.getElementsByTagName("input");
+			    for (var i = 0; i<inputs.length; i++) {
+			        if (!inputs[i].value){
+			            alert("Please fill all the inputs");
+			            return false;
+			         }
+			     }
+			     return true;
+			};
+		</script>
+	</body>
 
 
-
-?>
+</html>
--- delete.php
+++ delete.php
@@ -1,7 +1,18 @@
 <?php
+	include("database.php");
 	$selected = $_POST["formStudents"];
 	$count = count($selected);
+	$playerGroup = $connection->prepare("DELETE FROM tblPLAYER_GROUP WHERE PlayerID = :playerID");
+	$playerPractice = $connection->prepare("DELETE FROM tblPLAYER_PRACTICE WHERE PlayerID = :playerID");
+	$player = $connection->prepare("DELETE FROM tblPLAYER WHERE PlayerID = :playerID");
+	//$playerGroup ->bindPram(':playerID', $row);
 	foreach($selected as $row) {
+		$playerGroup ->bindParam(':playerID', $row);
+		$playerGroup -> execute();
+		$playerPractice -> bindParam(':playerID', $row);
+		$playerPractice -> execute();
+		$player -> bindParam(':playerID', $row);
+		$player -> execute();
 		echo $row;
 	}
 	//echo $selected[0];
